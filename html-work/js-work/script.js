cd00a96ef5aeb114db3cb6ef470d8b8e16cee756
Waleed Q <iamwaq@icloud.com>
2022-12-26 14:02:52 +0300

    More BS classes and the assignments

--- html-work/js-work/script.js
+++ html-work/js-work/script.js
@@ -1,1 +1,31 @@
 // alert("works?");
+
+function playAround(){
+    let art = [678, 223, 927, 7, 89];
+
+    // console.log(art);
+
+    // console.log(art[2]);
+
+    // for(let index = 0; index < art.length; index++){
+    //     console.log(art[index]);
+    // }
+
+
+    // Or Traverse through a for in
+    // for(let key in art){
+    //     console.log(art[key]);
+    // }
+
+    let people = {name: "Sarah", id: 721}
+
+    console.log(people["name"]);
+
+    for(let key in people){
+        console.log(people[key]);
+    }
+
+    console.log(typeof people);
+
+    // let {name, id} = people; 
+}
